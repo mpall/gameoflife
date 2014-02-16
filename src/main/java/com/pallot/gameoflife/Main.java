5c4f79df9d6d004d6732c4280ec081575a410fae
mpall <marcuspallot@hotmail.com>
2014-02-16 01:16:09 +0000

    x y coordinates

--- src/main/java/com/pallot/gameoflife/Main.java
+++ src/main/java/com/pallot/gameoflife/Main.java
@@ -14,16 +14,16 @@ public class Main {
 	}
 	public void run() throws IOException, InterruptedException {
 		setup();
-		//setupBlock();
-		//setupBlinker();
-		setupGlider(6);
-		//setupGlider(10);
-		setupGlider(12);
-		//setupGlider(20);
-		//setupLightWeightShip(7);
-		setupLightWeightShip(13);
-		//setupLightWeightShip(23);
-		setupLightWeightShip(29);
+		//setupBlock(0, 0);
+		//setupBlinker(0, 0);
+		setupGlider(6, 0);
+		//setupGlider(10, 0);
+		setupGlider(12, 0);
+		//setupGlider(20, 0);
+		//setupLightWeightShip(0, 7);
+		setupLightWeightShip(0, 13);
+		//setupLightWeightShip(0, 23);
+		setupLightWeightShip(0, 29);
 		
 		new Animator(thisGrid).interval(100).run();
 		//new Animator(thisGrid).interactiveMode().run();
@@ -33,50 +33,44 @@ public class Main {
 		setupGrid(hightAndWidth);
 	}
 	
-	private void setupLightWeightShip(int y) {
-		thisGrid.set((thisGrid.getHightAndWidth() * y) + 1, ALIVE);
-		thisGrid.set((thisGrid.getHightAndWidth() * y) + 2, ALIVE);
-		thisGrid.set((thisGrid.getHightAndWidth() * y) + 3, ALIVE);
-		thisGrid.set((thisGrid.getHightAndWidth() * y) + 4, ALIVE);
-		thisGrid.set((thisGrid.getHightAndWidth() * y) + thisGrid.getHightAndWidth(), ALIVE);
-		thisGrid.set((thisGrid.getHightAndWidth() * y) + thisGrid.getHightAndWidth() + 4, ALIVE);
-		thisGrid.set((thisGrid.getHightAndWidth() * y) + thisGrid.getHightAndWidth() * 2 + 4, ALIVE);
-		thisGrid.set((thisGrid.getHightAndWidth() * y) + thisGrid.getHightAndWidth() * 3 + 3, ALIVE);
-		thisGrid.set((thisGrid.getHightAndWidth() * y) + thisGrid.getHightAndWidth() * 3, ALIVE);
+	private int shift(int x, int y) {
+		return (thisGrid.getHightAndWidth() * y) + x;
 	}
 	
-	private void setupGlider() {
-		thisGrid.set(thisGrid.getHightAndWidth(), ALIVE);
-		thisGrid.set((thisGrid.getHightAndWidth() * 2) + 1, ALIVE);
-		thisGrid.set(2, ALIVE);
-		thisGrid.set(thisGrid.getHightAndWidth() + 2, ALIVE);
-		thisGrid.set((thisGrid.getHightAndWidth()* 2) + 2, ALIVE);
+	private void setupLightWeightShip(int x, int y) {
+		thisGrid.set(shift(x, y) + 1, ALIVE);
+		thisGrid.set(shift(x, y) + 2, ALIVE);
+		thisGrid.set(shift(x, y) + 3, ALIVE);
+		thisGrid.set(shift(x, y) + 4, ALIVE);
+		thisGrid.set(shift(x, y) + thisGrid.getHightAndWidth(), ALIVE);
+		thisGrid.set(shift(x, y) + thisGrid.getHightAndWidth() + 4, ALIVE);
+		thisGrid.set(shift(x, y) + thisGrid.getHightAndWidth() * 2 + 4, ALIVE);
+		thisGrid.set(shift(x, y) + thisGrid.getHightAndWidth() * 3 + 3, ALIVE);
+		thisGrid.set(shift(x, y) + thisGrid.getHightAndWidth() * 3, ALIVE);
 	}
 	
-	private void setupGlider(int offset) {
-		thisGrid.set(thisGrid.getHightAndWidth() + offset, ALIVE);
-		thisGrid.set((thisGrid.getHightAndWidth() * 2) + 1  + offset, ALIVE);
-		thisGrid.set(2  + offset, ALIVE);
-		thisGrid.set(thisGrid.getHightAndWidth() + 2  + offset, ALIVE);
-		thisGrid.set((thisGrid.getHightAndWidth()* 2) + 2  + offset, ALIVE);
+	private void setupGlider(int x, int y) {
+		thisGrid.set(shift(x, y) + thisGrid.getHightAndWidth(), ALIVE);
+		thisGrid.set(shift(x, y) + (thisGrid.getHightAndWidth() * 2) + 1, ALIVE);
+		thisGrid.set(shift(x, y) + 2, ALIVE);
+		thisGrid.set(shift(x, y) + thisGrid.getHightAndWidth() + 2, ALIVE);
+		thisGrid.set(shift(x, y) + (thisGrid.getHightAndWidth()* 2) + 2, ALIVE);
+	}
+	
+	private void setupBlinker(int x, int y) {
+		setupGrid(shift(x, y) + 5);
+		thisGrid.set(shift(x, y) + 11, ALIVE);
+		thisGrid.set(shift(x, y) + 12, ALIVE);
+		thisGrid.set(shift(x, y) + 13, ALIVE);
 	}
 
 
-
-	private void setupBlinker() {
-		setupGrid(5);
-		thisGrid.set(11, ALIVE);
-		thisGrid.set(12, ALIVE);
-		thisGrid.set(13, ALIVE);
-	}
-
-
-	private void setupBlock() {
-		setupGrid(4);
-		thisGrid.set(5, ALIVE);
-		thisGrid.set(6, ALIVE);
-		thisGrid.set(9, ALIVE);
-		thisGrid.set(10, ALIVE);
+	private void setupBlock(int x, int y) {
+		setupGrid(shift(x, y) + 4);
+		thisGrid.set(shift(x, y) + 5, ALIVE);
+		thisGrid.set(shift(x, y) + 6, ALIVE);
+		thisGrid.set(shift(x, y) + 9, ALIVE);
+		thisGrid.set(shift(x, y) + 10, ALIVE);
 	}
 	
 	private void setupGrid(int hightAndWidth) {
